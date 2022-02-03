427c1d622aed793215c83656a53a7c565bfcd3dc
yuichi <y.ichityzy@gmail.com>
2022-02-03 13:03:14 +0900

    java18

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.board.repository.Post;
 import com.example.board.repository.PostFactory;
 import com.example.board.repository.PostRepository;
+import com.example.board.validation.GroupOrder;
 
 /**
  * 掲示板のフロントコントローラー.
@@ -37,7 +38,7 @@ public class BoardController {
    * @return 一覧を設定したモデル
    */
    private Model setList(Model model) {
-       Iterable<Post> list = repository.findAll();
+	   Iterable<Post> list = repository.findByDeletedFalseOrderByUpdatedDateDesc();
        model.addAttribute("list", list);
        return model;
    }
@@ -50,7 +51,7 @@ public class BoardController {
     * @return テンプレート
     */
    @RequestMapping(value = "/create", method = RequestMethod.POST)
-   public String create(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+   public String create(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result, Model model) {
 	   if (!result.hasErrors()) {
 		   repository.saveAndFlush(PostFactory.createPost(form));
 		   model.addAttribute("form", PostFactory.newPost());
@@ -84,7 +85,7 @@ public class BoardController {
     * @return テンプレート
     */
    @RequestMapping(value = "/update", method = RequestMethod.POST)
-   public String update(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+   public String update(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result, Model model) {
 	   if (!result.hasErrors()) {
 		   Optional<Post> post = repository.findById(form.getId());
 		   repository.saveAndFlush(PostFactory.updatePost(post.get(), form));
--- src/main/java/com/example/board/repository/Post.java
+++ src/main/java/com/example/board/repository/Post.java
@@ -9,6 +9,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.example.board.validation.Group1;
+import com.example.board.validation.Group2;
+
 import lombok.Data;
 
 /**
@@ -27,20 +30,20 @@ public class Post {
 
     /** 投稿者 */
     @Column(length = 20, nullable = false)
-    @NotEmpty
-    @Size(min = 1, max = 20)
+    @NotEmpty(groups = Group1.class)
+    @Size(min = 1, max = 20, groups = Group2.class)
     private String author = null;
 
     /** タイトル */
     @Column(length = 20, nullable = false)
-    @NotEmpty
-    @Size(min = 1, max = 20)
+    @NotEmpty(groups = Group1.class)
+    @Size(min = 1, max = 20, groups = Group2.class)
     private String title = null;
 
     /** 内容 */
     @Column(length = 1000, nullable = false)
-    @NotEmpty
-    @Size(min = 1, max = 1000)
+    @NotEmpty(groups = Group1.class)
+    @Size(min = 1, max = 1000, groups = Group2.class)
     private String body = null;
 
     /** 登録日時 */
